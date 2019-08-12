03a704b86f2890eb970b6e18874491b42bc4e2ca
Richard <22475@ufp.edu.pt>
2019-08-12 20:52:35 +0100

    init project build

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+    type rootQuery {                 
+        events: [String!]!
+    }
+
+    type rootMutation {    
+
+    }
+
+    schema {
+        query: rootQuery
+        mutation: rootMutation
+    }
+  
+  
+  `),                     // where to find the schemas, queries, result...etc
+    rootValue: {}
+}));
+
+
+
+app.listen(3000);
