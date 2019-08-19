998768c51a8499c0b67baeca276df47f93d6dda7
Richard Pereira <richard.inthenet@gmail.com>
2019-08-19 14:30:16 +0100

    Creating a new object Event using MongoDB schema

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require("mongoose");
+const Event = require('./models/event');
 
 const app = express();
-
-const events = []; //temp until database
 
 app.use(bodyParser.json());
 
@@ -45,16 +44,22 @@ app.use('/graphql', graphqlHttp({
             return events;
         },
         createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
+            const event = new Event({
                 title: args.evenInput.title,
                 description: args.evenInput.description,
                 price: +args.evenInput.price,
-                date: args.evenInput.date
-            };
-            events.push(event) // Insert new events to the Event List
-
-            return event;
+                date: new Date(args.evenInput.date)
+            });
+            return event // wait to complete
+                .save()
+                .then(result => {
+                    console.log(result);
+                    return { ...result._doc };
+                })
+                .catch(err => {
+                    console.log(err);
+                    throw err;
+                });
         }
     },
     graphiql: true //UI 
@@ -62,17 +67,10 @@ app.use('/graphql', graphqlHttp({
 
 );
 
-// mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:DqxJRduDF5CDKhnf@eventbooking-nznmx.mongodb.net/events-rect-dev?retryWrites=true&w=majority`,{ useNewUrlParser: true })
-//                                 .then( () => {
-//                                     app.listen(3000);
-//                                 }).catch(err => {
-//                                     console.log(err);
-//                                 });
-
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@eventbooking-nznmx.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,{ useNewUrlParser: true })
-                                .then( () => {
-                                    app.listen(3000);
-                                }).catch(err => {
-                                    console.log(err);
-                                });
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@eventbooking-nznmx.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true })
+    .then(() => {
+        app.listen(3000);
+    }).catch(err => {
+        console.log(err);
+    });
 
