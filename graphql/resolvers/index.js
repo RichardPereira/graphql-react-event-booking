fe565a6be75c2854bd5d91286a4fe8479ea8feca
Richard Pereira <richard.inthenet@gmail.com>
2019-08-24 16:25:02 +0100

    changed to async functions

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -11,6 +11,7 @@ const events = async eventIds => {
             return {
                 ...event._doc,
                 _id: event.id,
+                date: new Date(event._doc.date).toISOString(),
                 creator: user.bind(this, event._doc.creator)
             };
         });
@@ -21,42 +22,43 @@ const events = async eventIds => {
 };
 
 //fetching User by their Id
-const user = userId => {
-    return User.findById(userId)
-        .then(user => {
-            return {
-                ...user._doc,
-                id: user.id,
-                password: null,
-                createdEvents: events.bind(this, user._doc.createdEvents)
-            };
-        })
-        .catch(err => {
-            throw err;
-        });
+const user = async userId => {
+    try {
+        const user = await User.findById(userId);
+        return {
+            ...user._doc,
+            id: user.id,
+            password: null,
+            createdEvents: events.bind(this, user._doc.createdEvents)
+        };
+    }
+    catch (err) {
+        throw err;
+    }
 };
 
 
 module.exports = {
-    events: () => {
-        return Event.find() // asyc event: GraphQl need to wait to finished           
-            .then(
-                events => {
-                    return events.map(event => {
-                        return {
-                            ...event._doc,
-                            _id: event.id,
-                            password: null,
-                            creator: user.bind(this, event._doc.creator)
-                        };
-                    });
-                })
-            .catch(err => {
-                console.log(err);
-                throw err;
-            })
+    events: async () => {
+        try {
+            const events = await Event.find() // asyc event: GraphQl need to wait to finished           
+                ;
+            return events.map(event => {
+                return {
+                    ...event._doc,
+                    _id: event.id,
+                    password: null,
+                    date: new Date(event._doc.date).toISOString(),
+                    creator: user.bind(this, event._doc.creator)
+                };
+            });
+        }
+        catch (err) {
+            console.log(err);
+            throw err;
+        }
     },
-    createEvent: (args) => {
+    createEvent: async (args) => {
         const event = new Event({
             title: args.evenInput.title,
             description: args.evenInput.description,
@@ -65,53 +67,45 @@ module.exports = {
             creator: '5d5e977f0eef6436ec2e3638'
         });
         let createdEvent;
-        return event // wait to complete
-            .save()
-            .then(result => {
-                createdEvent = {
-                    ...result._doc,
-                    _id: result._doc._id.toString(),
-                    creator: user.bind(this, result._doc.creator)
-                };
-                return User.findById('5d5e977f0eef6436ec2e3638') // dummy temp Id
-            })
-            .then(user => {
-                if (!user) {
-                    throw new Error('User not found.');
-                }
-                user.createdEvents.push(event) // associate the Event to the User
-                return user.save();
-            })
-            .then(result => {
-                return createdEvent;
-               
-            })
-            .catch(err => {
-                console.log(err);
-                throw err;
-            });
+        try {
+            const result = await event.save();
+            createdEvent = {
+                ...result._doc,
+                date: new Date(event._doc.date).toISOString(),
+                _id: result._doc._id.toString(),
+                creator: user.bind(this, result._doc.creator)
+            };
+            const user = await User.findById('5d5e977f0eef6436ec2e3638') // dummy temp Id
+                ;
+            if (!user) {
+                throw new Error('User not found.');
+            }
+            user.createdEvents.push(event); // associate the Event to the User
+            const result_1 = await user.save();
+            return createdEvent;
+        }
+        catch (err) {
+            console.log(err);
+            throw err;
+        }
     },
-    createUser: args => {
-        return User.findOne({ email: args.userInput.email })
-            .then(user => {
-                if (user) {
-                    throw new Error('User exists already.')
-                }
-                return bcrypt
-                    .hash(args.userInput.password, 12);
-            })
-            .then(hashedPassword => {
-                const user = new User({
-                    email: args.userInput.email,
-                    password: hashedPassword
-                });
-                return user.save();
-            })
-            .then(result => {
-                return { ...result._doc, password: null };
-            })
-            .catch(err => {
-                throw err;
+    createUser: async args => {
+        try {
+            const user = await User.findOne({ email: args.userInput.email });
+            if (user) {
+                throw new Error('User exists already.');
+            }
+            const hashedPassword = await bcrypt
+                .hash(args.userInput.password, 12);
+            const user_1 = new User({
+                email: args.userInput.email,
+                password: hashedPassword
             });
+            const result = await user_1.save();
+            return { ...result._doc, password: null };
+        }
+        catch (err) {
+            throw err;
+        }
     }
 };
