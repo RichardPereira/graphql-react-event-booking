4e7c6273afe036afc981c54bce6d3af7b5f0f821
Richard Pereira <richard.inthenet@gmail.com>
2019-08-28 15:48:01 +0100

    Refactoring Booking return

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -15,6 +15,17 @@ const transformEvent = event => {
     }
 };
 
+const transformBooking = booking => {
+    return {
+        ...booking._doc,
+        _id: booking.id,
+        user: user.bind(this, booking._doc.user),
+        event: singleEvent.bind(this, booking._doc.event),
+        createdAt: dateToString(booking._doc.createdAt),
+        updatedAt: dateToString(booking._doc.updatedAt)
+    }
+}
+
 /**
  * Fetching all the events
  * @param {String} eventIds 
@@ -82,14 +93,7 @@ module.exports = {
         try {
             const bookings = await Booking.find();
             return bookings.map(booking => {
-                return {
-                    ...booking._doc,
-                    _id: booking.id,
-                    user: user.bind(this, booking._doc.user),
-                    event: singleEvent.bind(this, booking._doc.event),
-                    createdAt: dateToString(booking._doc.createdAt),
-                    updatedAt: dateToString(booking._doc.updatedAt)
-                };
+                return transformBooking(booking);
             });
 
         } catch (err) {
@@ -153,14 +157,7 @@ module.exports = {
 
             });
             const result = await booking.save();
-            return {
-                ...result._doc,
-                _id: result.id,
-                createdAt: dateToString(result._doc.createdAt),
-                updatedAt: dateToString(result._doc.updatedAt),
-                user: user.bind(this, booking._doc.user),
-                event: singleEvent.bind(this, booking._doc.event),
-            };
+            return transformBooking(result);
 
         } catch (error) {
             throw error
