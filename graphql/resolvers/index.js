3ce23f390ed17cde1d0357e31357c21f683acb6e
Richard Pereira <richard.inthenet@gmail.com>
2019-08-28 15:35:52 +0100

    refactoring  return  Event

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,6 +4,16 @@ const Event = require('../../models/event');
 const User = require('../../models/user');
 const Booking = require('../../models/booking');
 
+const transformEvent = event => {
+    return {
+        ...event._doc,
+        _id: event.id,
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, event._doc.creator)
+    }
+
+};
+
 /**
  * Fetching all the events
  * @param {String} eventIds 
@@ -12,12 +22,7 @@ const events = async eventIds => {
     try {
         const events = await Event.find({ _id: { $in: eventIds } });
         return events.map(event => {
-            return {
-                ...event._doc,
-                _id: event.id,
-                date: new Date(event._doc.date).toISOString(),
-                creator: user.bind(this, event._doc.creator)
-            };
+            return transformEvent(event);
         });
     }
     catch (err) {
@@ -32,12 +37,7 @@ const singleEvent = async eventId => {
 
     try {
         const event = await Event.findById(eventId);
-        return {
-            ...event._doc,
-            _id: event.id,
-            date: new Date(event._doc.date).toISOString(),
-            creator: user.bind(this, event._doc.creator)
-        };
+        return transformEvent (event);
     } catch (error) {
         throw error;
     }
@@ -69,13 +69,7 @@ module.exports = {
             const events = await Event.find() // asyc event: GraphQl need to wait to finished           
                 ;
             return events.map(event => {
-                return {
-                    ...event._doc,
-                    _id: event.id,
-                    password: null,
-                    date: new Date(event._doc.date).toISOString(),
-                    creator: user.bind(this, event._doc.creator)
-                };
+                return transformEvent (event);
             });
         }
         catch (err) {
@@ -112,12 +106,8 @@ module.exports = {
         let createdEvent;
         try {
             const result = await event.save();
-            createdEvent = {
-                ...result._doc,
-                date: new Date(event._doc.date).toISOString(),
-                _id: result._doc._id.toString(),
-                creator: user.bind(this, result._doc.creator)
-            };
+            createdEvent = transformEvent(result);
+
             const creator = await User.findById('5d61589462b8a8280cc9cb61') // dummy temp Id
                 ;
             if (!creator) {
@@ -178,11 +168,7 @@ module.exports = {
     cancelBooking: async args => {
         try {
             const booking = await Booking.findById(args.bookingId).populate('event');
-            const event = {
-                ...booking.event._doc,
-                id: booking.event.id,
-                creator: user.bind(this, booking.event._doc.creator)
-            };
+            const event = transformEvent(booking.event);
             await Booking.deleteOne({ _id: args.bookingId });
             return event;
         } catch (error) {
