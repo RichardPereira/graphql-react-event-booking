3e61d764cdf0eab8595b7369e28928a5f38fdee1
Richard Pereira <richard.inthenet@gmail.com>
2019-08-30 10:55:22 +0100

    Can used authContext in all the childs elements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,26 @@ import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
 import MainNavigation from './components/Navigation/MainNavigation';
+import AuthContext from './context/auth-contex';
 
 function App() {
   return (
-   
+
     <BrowserRouter>
-      <MainNavigation />
-      <main className="main-content">
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          {/* <Route path="/" component={null} /> */}
-          <Route path="/auth" component={AuthPage}  />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
-      </main>
+      <React.Fragment>
+        <AuthContext.Provider >
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              {/* <Route path="/" component={null} /> */}
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </AuthContext.Provider>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
