719e4ab4bdc9510f4e76813bdbe380e66cdb0799
Richard Pereira <richard.inthenet@gmail.com>
2019-08-30 10:42:53 +0100

    changed port and handle CORS  response

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const mongoose = require("mongoose");
-const app = express();
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
 const isAuth = require('./middleware/is-auth');
 
+const app = express();
+
 app.use(bodyParser.json());
+
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS'){
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.use(isAuth); //middleware 
 
@@ -20,7 +31,7 @@ app.use('/graphql', graphqlHttp({                     // one EndPoint
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@eventbooking-nznmx.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true })
     .then(() => {
-        app.listen(3000);
+        app.listen(8000);
     }).catch(err => {
         console.log(err);
     });
