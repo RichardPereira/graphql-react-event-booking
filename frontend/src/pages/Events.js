4f347290643bbb6dee2577dc0debdf1958536083
Richard Pereira <richard.inthenet@gmail.com>
2019-09-02 21:23:48 +0100

    Diplay the fields to the creating of an event

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -24,8 +24,35 @@ class EventsPage extends Component {
         return (
             <React.Fragment>
                 {this.state.creating && <Backdrop />}
-                {this.state.creating && <Modal title="Add Event" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
-                </Modal>}
+                {this.state.creating && (
+                <Modal 
+                    title="Add Event" 
+                    canCancel 
+                    canConfirm 
+                    onCancel={this.modalCancelHandler} 
+                    onConfirm={this.modalConfirmHandler}
+                > 
+                    <form>
+                        <div className="form-control">
+                            <label htmlFor="title"> Title</label>
+                            <input type="text" id="title"></input>
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="price"> Price</label>
+                            <input type="number" id="price"></input>
+                        </div>
+
+                        <div className="form-control">
+                            <label htmlFor="date"> Date</label>
+                            <input type="date" id="date"></input>
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="description">Description</label>
+                            <textarea id="descrition" rows="3"/>
+                        </div>
+                    </form>
+                </Modal>
+        )}
                 <div className="events-control">
                     <p>share your own events!</p>
                     <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
